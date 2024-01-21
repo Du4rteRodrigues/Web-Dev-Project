40b80dcb9f9beb90b0ba687a509abcf26cfb89b8
Du4rteRodrigues <duarte.m.rodrigues@sapo.pt>
2024-01-21 17:21:19 +0000

    fixed posts

--- public/Javascript/home.js
+++ public/Javascript/home.js
@@ -1,3 +1,5 @@
+//const db = require('./database')
+
 document.addEventListener('DOMContentLoaded', function () {
   var openPopupBtn = document.getElementById('openPopupBtn');
   var closePopupBtn = document.getElementById('closePopupBtn');
@@ -5,7 +7,12 @@ document.addEventListener('DOMContentLoaded', function () {
   var profileBtn = document.getElementById('profileBtn');
   var popup = document.getElementById('popup');
 
-  window.onload= createPost
+  window.onload= onLoad
+
+  function onLoad(){
+    createPosts()
+    updateContentHeight()
+  }
 
   // Abrir o pop-up
   openPopupBtn.addEventListener('click', function () {
@@ -32,9 +39,22 @@ document.addEventListener('DOMContentLoaded', function () {
     popup.style.display = 'none';
   });
 
+function changeNumberSize(num){
+  if(num.value == 0){
+    num.style.width = '35px'
+  }else if (num.value/10 <= 1){
+    num.style.width = `50px`
+  }
+  else if (num.value/10 <= 10){
+    num.style.width = `70px`
+  }else if (num.value/10 >= 100){
+    num.style.width = `100px`
+  }
+  return num.style.width
+}
 
-function createPost(){
-  for(var i=0; i<3;i++){
+function createPosts(){
+  for(var i=0; i<10;i++){
       var post = document.createElement('div');
       var infoDiv = document.createElement('div')
       var contentDiv = document.createElement('div')
@@ -44,9 +64,11 @@ function createPost(){
       var user = document.createElement('textarea')
       var content = document.createElement('textarea')
       var likes = document.createElement('input')
+      var commentNum = document.createElement('input')
 
       var likeImg = document.createElement('img')
       var dislikeImg = document.createElement('img')
+      var commentsImg = document.createElement('img')
       var br = document.createElement("br")
 
       post.className='post';
@@ -64,25 +86,37 @@ function createPost(){
       title.className = 'post-title'
       title.id = `post-title-${i}`
       title.readOnly = true
-      title.placeholder = '80 chars'
+      title.value = ''
+      //80 chars
 
       user.className = 'post-user'
       user.id = `post-user-${i}`
       user.readOnly = true
-      user.placeholder = '14 chars'
+      user.value= ''
+      //17 chars
 
       content.id = `post-content-${i}`
       content.className = 'post-content'
-      content.readOnly = false
-      content.value = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod hic nam autem quae vero blanditiis ipsum laudantium commodi facere rem! Omnis cupiditate velit similique facilis harum. Tenetur eum quam ad porro earum fugiat ullam, pariatur tempore quas iure nobis laborum ratione harum laudantium commodi officia ipsam reiciendis eius, magni minus impedit illo omnis hic et. Culpa ipsa exercitationem, perspiciatis cumque tenetur eveniet doloremque eum, obcaecati optio omnis minus id numquam quo officia fugit totam similique minima quod quae. Aperiam a iusto delectus magni eligendi quod deserunt quos non id nemo repellendus qui officia amet enim, repellat dolore quas dolor accusamus sunt. Consectetur ut illo fugiat porro, nisi voluptatibus, vel eveniet minus odio nam nulla voluptates maiores non itaque doloribus, quis nobis asperiores cum! Quidem unde temporibus rem. Possimus, consequatur quam? Quia, nesciunt rem. Veniam sequi tenetur, eveniet hic deserunt corrupti et eligendi itaque quia sapiente amet dolor nihil laborum pariatur facilis repellendus! Quidem porro accusamus aliquid exercitationem odio, obcaecati consectetur eveniet ut commodi veritatis pariatur voluptates ipsa laudantium laborum facilis dolor. Consectetur similique suscipit natus voluptatem dignissimos obcaecati id velit! Sequi, itaque aut harum earum laboriosam totam, laborum saepe magnam quod obcaecati recusandae reprehenderit ea voluptatem nam quasi, provident in rerum vitae? Vero tempora consequuntur laudantium pariatur iusto, ullam eligendi dolore aliquid necessitatibus enim alias quis quaerat odit officiis! Reprehenderit asperiores dolores, consequatur minus laborum atque debitis cumque autem officiis amet officia soluta modi quidem perspiciatis explicabo error ex maxime rem velit! Cum, nihil excepturi magni incidunt quas provident error, ea sunt assumenda exercitationem quam, eum ab. Quo consequuntur numquam laboriosam veritatis rem ducimus, corporis error culpa iusto voluptatum odit dolores neque quaerat quis perferendis fugit et voluptatem cum inventore optio alias dicta possimus labore? Nam distinctio dolorum non obcaecati facilis accusamus alias provident aut consequatur reprehenderit voluptas blanditiis quia iusto neque impedit quisquam, consectetur et hic incidunt, ab praesentium officia expedita laboriosam? Dolores, praesentium. Et dolores asperiores repellat quas harum quidem maxime vitae, sapiente expedita odit doloribus, nisi sed, neque assumenda? Tempore exercitationem maiores ipsum repellat voluptatibus ut recusandae magni debitis corrupti, aperiam perferendis odio consequatur dolorem perspiciatis distinctio quis. Minima soluta a velit, deleniti reiciendis cupiditate minus distinctio! Ut, tempora! Laudantium architecto ipsa, vitae rem vel eius temporibus sunt fuga optio provident ad fugit rerum! Repudiandae autem aspernatur possimus similique libero minus tempora. Enim rerum veniam vel non nesciunt? Animi odit qui, reprehenderit eligendi non asperiores deserunt adipisci aliquam, quaerat a et necessitatibus impedit autem quis nobis ullam exercitationem suscipit provident, nisi nihil labore doloribus quasi! Voluptate eius quaerat incidunt non praesentium porro autem, alias esse minus nisi? Debitis fugiat quos perspiciatis, blanditiis sapiente esse dolore quas porro aperiam voluptatem provident laudantium repudiandae assumenda sint. Praesentium sunt, at nisi ab velit culpa debitis saepe et vel pariatur ea quidem eos animi, odio nesciunt voluptate doloremque! Dolor, laborum voluptatibus? Velit, ut facilis quam molestiae commodi consequatur deleniti. Beatae inventore saepe, minima, molestiae voluptatibus aspernatur eaque excepturi explicabo tenetur accusantium veritatis fuga architecto dolorem dignissimos fugit, nemo nam suscipit FIMMM'
-      content.placeholder='900 chars'
-
+      content.readOnly = true
+      content.value = ''
+      
       likes.id = `post-likes-${i}`
       likes.className= 'post-engagment'
       likes.value = 0
       likes.readOnly = true
       likes.disabled= "yes"
       likes.type = 'number'
+
+      if(likes.value == 0){
+        likes.style.width = '35px'
+      }else if (likes.value/10 <= 1){
+        likes.style.width = `50px`
+      }
+      else if (likes.value/10 <= 10){
+        likes.style.width = `70px`
+      }else if (likes.value/10 >= 99){
+        likes.style.width = `100px`
+      }
 
       likeImg.src ='../Images/up.png'
       likeImg.id = `post-like-img-${i}`
@@ -94,19 +128,49 @@ function createPost(){
       dislikeImg.className ='post-down'
       dislikeImg.addEventListener("click", function() { changeLikes('down', this.id);});
 
+      commentNum.id = `post-comments-${i}`
+      commentNum.className= 'post-comments'
+      commentNum.value = 0
+      commentNum.readOnly = true
+      commentNum.disabled= "yes"
+      commentNum.type = 'number'
+      if(commentNum.value == 0){
+        commentNum.style.width = '35px'
+      }else if (commentNum.value/10 <= 1){
+        commentNum.style.width = `50px`
+      }
+      else if (commentNum.value/10 <= 10){
+        commentNum.style.width = `70px`
+      }else if (commentNum.value/10 >= 100){
+        commentNum.style.width = `100px`
+      }
+
+      commentsImg.src ='../Images/comments.png'
+      commentsImg.id = `post-comments-img-${i}`
+      commentsImg.className ='post-comments-img'
+      commentsImg.addEventListener("click", function() { changeComments('up', this.parentNode.id);});
+
       infoDiv.appendChild(user)
       infoDiv.appendChild(title)
       contentDiv.appendChild(content)
       engagmentDiv.appendChild(likes)
       engagmentDiv.appendChild(dislikeImg)
       engagmentDiv.appendChild(likeImg)
+      engagmentDiv.appendChild(commentsImg)
+      engagmentDiv.appendChild(commentNum)
       post.appendChild(infoDiv)
       post.appendChild(contentDiv)
       post.appendChild(engagmentDiv)
-
       document.getElementsByTagName('section')[0].appendChild(post);
       document.getElementsByTagName('section')[0].appendChild(br);
   }
+}
+
+function changeComments(type, element){
+  const btn = document.getElementById(element)
+  const post = document.getElementById(btn.parentNode.id)
+  const commentsNum = post.querySelector('.post-comments')
+  commentsNum.stepUp(1)
 }
 
 function changeLikes(type, element){
@@ -121,12 +185,14 @@ function changeLikes(type, element){
       if(upBtn.getAttribute('src') == '../Images/up.png' && downBtn.getAttribute('src') == '../Images/down.png'){
           upBtn.src = '../Images/up-clicked.png'
           likes.stepUp(1)
-          likes.style.color = "green"
+        likes.style.width = changeNumberSize(likes)
+        likes.style.color = "#22B14C"
       }
 
       else if(upBtn.getAttribute('src') == '../Images/up-clicked.png'){
           upBtn.src = '../Images/up.png'
           likes.stepDown(1)
+          likes.style.width = changeNumberSize(likes)
           likes.style.color = "black"
       }
 
@@ -134,7 +200,8 @@ function changeLikes(type, element){
           downBtn.src = '../Images/down.png'
           upBtn.src = '../Images/up-clicked.png'
           likes.stepUp(2)
-          likes.style.color = "green"
+          likes.style.width = changeNumberSize(likes)
+          likes.style.color = "#22B14C"
       }
 
       }
@@ -143,12 +210,14 @@ function changeLikes(type, element){
       if(downBtn.getAttribute('src') == '../Images/down.png' && upBtn.getAttribute('src') == '../Images/up.png'){
           downBtn.src = '../Images/down-clicked.png'
           likes.stepDown(1)
+          likes.style.width = changeNumberSize(likes)
           likes.style.color = "red"
       }
 
       else if(downBtn.getAttribute('src') == '../Images/down-clicked.png'){
           downBtn.src = '../Images/down.png'
           likes.stepUp(1)
+          likes.style.width = changeNumberSize(likes)
           likes.style.color = "black"
       }
 
@@ -156,14 +225,30 @@ function changeLikes(type, element){
           downBtn.src = '../Images/down-clicked.png'
           upBtn.src = '../Images/up.png'
           likes.stepDown(2)
+          likes.style.width = changeNumberSize(likes)
           likes.style.color = "red"
       }
       }
 }
 
+function updateContentHeight(){
+  var allPosts = document.querySelectorAll('.post');
+  allPosts.forEach(function(post) {
+  var elementsInDiv = post.querySelectorAll('.post-content');
+  updateSize(elementsInDiv);
+});
+}
+
+// Example: Log the text content of each element
+function updateSize(elements) {
+    elements.forEach(function(element) {
+      element.style.height = element.scrollHeight+ 'px'
+    });
+}
 
 
 function aboutUs(){
 window.location.replace("http://localhost:8888/about")
 }
 });
+
